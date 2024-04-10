a31cd6898052c6a9a1d4d73d092c46aca61c2bb4
Satyam Rai <satyamrai553@gamil.com>
2024-04-11 01:01:29 +0530

    connectin to atlas

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB();
+
+
+
+/*
+
+;(async()=> {
+    try{
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    }catch(error){
+        console.error("ERROE: ",error)
+        throw err
+    }
+})()
+
+*/
